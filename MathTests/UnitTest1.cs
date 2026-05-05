d0e9285d30040b45b6e4327a8c88becbd179b7cc
Nhunguy <munnguy@gmail.com>
2026-05-05 12:01:45 +0700

    feat: implement Newton's method for SquareRoot to handle value ranges

--- MathTests/UnitTest1.cs
+++ MathTests/UnitTest1.cs
@@ -21,4 +21,21 @@ public class UnitTest1
         //  Verify  the  result:
         Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 100);
     }
+    
+    [TestMethod]
+    public void RooterValueRange() {
+        // Create an instance to test.
+        Rooter rooter = new Rooter();
+
+        // Try a range of values.
+        for (double expected = 1e-8; expected < 1e+8; expected *= 3.2) {
+            RooterOneValue(rooter,  expected);
+        }
+    }
+
+    private  void  RooterOneValue(Rooter  rooter,  double  expectedResult) {
+        double input = expectedResult * expectedResult; 
+        double  actualResult  =  rooter.SquareRoot(input);
+        Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 1000);
+    }
 }
--- MyMath/Rooter.cs
+++ MyMath/Rooter.cs
@@ -1,7 +1,15 @@
 ﻿namespace MyMath;
 
 public class Rooter {
-    public double SquareRoot(double input) {
-        return input / 2; 
+    public double SquareRoot(double input)
+    {
+        double result = input;
+        double  previousResult  =  -input;
+        while  (Math.Abs(previousResult  -  result)  >  result  /  1000)
+        {
+            previousResult  =  result;
+            result = result - (result * result - input) / (2 * result);
+        }
+        return result;
     }
 }
